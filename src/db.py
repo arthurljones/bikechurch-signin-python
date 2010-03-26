1df175dccaabaaac2f740e14390950aee48b5d63
Arthur Jones <arthur.l.jones@gmail.com>
2010-03-26 05:41:40 +0000

    * Now using sqlalchemy as the object relational mapper
    git-svn-id: https://ajones.svn.cloudforge.com/bikechurch_signin/trunk@38 736f2b9f-6243-40aa-8f55-659c37f65487

--- src/db.py
+++ src/db.py
@@ -1,289 +1,152 @@
  # -*- coding: utf-8 -*-
  
-#MySQLdb uses ImmutableSet, which is deprecated, so python complains by default
-import warnings
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", DeprecationWarning)
-    import MySQLdb
+import sys
 
-import sys, re
-from datetime import date, timedelta
+from sqlalchemy import (create_engine, MetaData, Column, Table, Integer, Unicode,
+	Index, ForeignKeyConstraint, Date, DateTime, Time)
+from sqlalchemy.dialects.mysql.base import MSEnum 
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-def SearchAndExtract(regex, string, group = 0):
-	result = regex.search(string, re.IGNORECASE)
-	if result is None:
-		return (string, None)
-	else:
-		newString = string[:result.start()] + string[result.end():]
-		string = newString
-		return (newString, result.group(group))
+Base = declarative_base()
 
-class ColumnDef:
-	reUnsigned = re.compile("\s*unsigned\s*")
-	reZerofill = re.compile("\s*zerofill\s*")
-	reParens = re.compile("\s*\((.*)\)\s*")
-	reCharset = re.compile("\s*character set (\w+)\s*")
-	reCollation = re.compile("\s*collation (\w+)\s*")
+class Person(Base):
+	__tablename__ = 'people'
+	__table_args__ = { 'mysql_engine' : 'InnoDB', 'mysql_charset' : 'utf8' }
 	
-	def __init__(self, row):
-		
-		self.name = row[0]
-		typeLine = row[1].lower()
-		
-		typeLine, self.unsigned  = SearchAndExtract(ColumnDef.reUnsigned, typeLine)
-		typeLine, self.zerofill  = SearchAndExtract(ColumnDef.reZerofill, typeLine)
-		typeLine, parensPart     = SearchAndExtract(ColumnDef.reParens, typeLine, 1)
-		typeLine, self.charset   = SearchAndExtract(ColumnDef.reCharset, typeLine, 1)
-		typeLine, self.collation = SearchAndExtract(ColumnDef.reCollation, typeLine, 1)
-		
-		self.type = typeLine
-		
-		self.unsigned = self.unsigned is None
-		self.zerofill = self.zerofill is None
-		
-		self.enumValues = None
-		self.precision = None
-		self.length = None
-		
-		if parensPart is not None:
-			if self.type == "enum" or self.type == "set":
-				self.enumValues = [word.strip() for word in parensPart.split(",")]
-			elif self.type == "float" or self.type == "double":
-				values = parensPart.split(",")
-				self.length = int(values[0])
-				self.precision = int(values[1])
-			else:
-				self.length = int(parensPart)
-			
-		self.nullAllowed = row[2]
-		self.key = row[3]
-		self.default = row[4]
-		self.extra = row[5]
-
-class RowObject:
-	#TODO: Create methods to validate params
-	def __init__(self, tableName, columnDefs):
-		self.columns = columnDefs
-		self.table = tableName
-		self.values = dict((col.name, col.default) for col in self.columns)
-		
-	def __getitem__(self, itemName):
-		return self.values[itemName]
-		
-	def __setitem__(self, itemName, value):
-		if itemName in self.values:
-			self.values[itemName] = value
-							
-	def FromQuery(self, query):
-		for column in self.columns:
-			self[column.name] = query[0]
-			query = query[1:]
-		if len(query) != 0:
-			print("RowObject.FromQuery discarded excess column values: {0}".format(query))
-			raise ValueError("Excess column values")
-		return self
-
-class TempRowObject:
-	def __init__(self, columnNames, row):
-		self.values = {}
-		for columnName in columnNames:
-			self.values[columnName] = row[0]
-			row = row[1:]
-		if len(row) != 0:
-			print("TempRowObject.FromQuery discarded excess column values: {0}".format(row))
-			raise ValueError("Excess column values")
-			
-	def __getitem__(self, itemName):
-		return self.values[itemName]
-
-class Connection:
-	def __init__(self):
-		self.connection = MySQLdb.connect(
-			host = "localhost", user = "signin", passwd = "signin", db = "signin_db")
-		self.cursor = self.connection.cursor()
-		
-		self.cursor.execute("SHOW TABLES;")
-		tables = [table[0] for table in self.cursor.fetchall()]
-		
-		self.tables = {}
-		for table in tables:
-			self.cursor.execute("SHOW COLUMNS FROM {0}".format(table))
-			self.tables[table] = []
-			for columnDefs in self.cursor.fetchall():
-				self.tables[table].append(ColumnDef(columnDefs))
-			
-	def __del__(self):
-		self.cursor.close()
-		self.connection.close()
-
-	def Commit(self):
-		self.connection.commit()
-		
-	def Rollback(self):
-		self.connection.rollback()
-
-	def Insert(self, rowObject):
-		'''Inserts values in rowObject into the the table specified by rowObject'''
-		valueNames = "("
-		valuesSubs = "("
-		valuesList = []
-		for value in rowObject.values.keys():
-			#Don't insert a value in the 'id' column, since it autoincrements
-			if value == "id":
-				continue
-			valueNames += "{0}, ".format(value)
-			valuesSubs += "%s, "
-			valuesList.append(rowObject.values[value])
-		valueNames = valueNames[:-2] + ")"
-		valuesSubs = valuesSubs[:-2] + ")"
-		valuesList = tuple(valuesList)
-		self.cursor.execute("INSERT INTO {0} {1} VALUES {2};"
-			.format(rowObject.table, valueNames, valuesSubs), valuesList)
-			
-		rowObject["id"] = self.connection.insert_id()
-			
-	def Update(self, rowObject):
-		'''Updates values of the object rowObject in the table specified by rowObject'''
-		setString = ""
-		valuesList = []
-		for value in rowObject.values.keys():
-			#id shouldn't change when updating
-			if value == "id":
-				continue
-			setString += "{0} = %s, ".format(value)
-			valuesList.append(rowObject.values[value])
-		setString = setString[:-2]
-		valuesList.append(rowObject["id"])
-		valuesList = tuple(valuesList)
-		self.cursor.execute("UPDATE {0} SET {1} WHERE id = %s;"
-			.format(rowObject.table, setString), valuesList)
+	id		= Column(Integer, primary_key = True, unique = True, nullable = False)
+	firstName	= Column(Unicode(64), nullable = False)
+	lastName 	= Column(Unicode(64))
+	memberInfo	= relationship('Member', backref = 'person', uselist = False)
+	shoptimes	= relationship('Shoptime', backref = 'person')
+	occupantInfo	= relationship('ShopOccupant', backref = 'person', uselist = False)
+	bikes		= relationship('Bike', backref = 'owner')
 	
-	'''TODO: Combine these query types. The only distinction is the use of RowObject, which
-		allows editing and updating. Using full names for complex queries should allow
-		editing and then updates, but the class that handles that still needs writing.
-	'''
-	def SimpleQuery(self, table, where, order = "", args = (), multipleResults = True):
-		query = "SELECT * FROM {0} WHERE {1}".format(table, where)
-		if order: query += "ORDER BY {0}".format(order)
-		query += ";"
-		self.cursor.execute(query, args)
-		if multipleResults: 
-			return [self.EmptyRow(table).FromQuery(q) for q in self.cursor.fetchall()]
-		else:
-			if self.cursor.rowcount == 0 :
-				return None
-			else: 
-				return self.EmptyRow(table).FromQuery(self.cursor.fetchone())
-	
-	def ComplexQuery(self, fields, tables = (), join = "", on = "", where = "", order = "", args = (), stripPrefixes = True):
-		query = "SELECT {0}".format(", ".join(fields))
-		if tables: query += " FROM {0}".format((" {0} ".format(join)).join(tables))
-		if on: query += " ON {0}".format(on)
-		if where: query += " WHERE {0}".format(where)
-		if order: query += " ORDER BY {0}".format(order)
-		query += ";"
+	def Name(self):
+		return "{0} {1}".format(self.firstName, self.lastName)
 		
-		self.cursor.execute(query, args)
-		if (stripPrefixes):
-			fields = [field.split(".")[-1] for field in fields]
-			
-		return [TempRowObject(fields, row) for row in self.cursor.fetchall()]
+	def __repr__(self):
+		return "<{0}> {1}".format(self.__class__.__name__, self.Name())
+
+Index('idx_fullName', Person.__table__.c.firstName, Person.__table__.c.lastName, unique = True)
+
+class Member(Base):
+	__tablename__ = 'members'
+	__table_args__ = (
+		ForeignKeyConstraint(['personID'], ['people.id'],
+			ondelete = 'CASCADE', onupdate = 'CASCADE'), 
+		{ 'mysql_engine' : 'InnoDB', 'mysql_charset' : 'utf8' }
+	)
+		
+	id		= Column(Integer, primary_key = True, unique = True, nullable = False)
+	personID	= Column(Integer, nullable = False, unique = True, index = True)
+	startDate	= Column(Date, nullable = False)
+	endDate		= Column(Date)
+	streetAddress	= Column(Unicode(128))
+	emailAddress	= Column(Unicode(64))
+	phoneNumber	= Column(Unicode(32))
+	donation	= Column(Integer, nullable = False)
+	notes		= Column(Unicode(200))
+
+shoptimeChoices = [
+	'shoptime',
+	'parts',
+	'worktrade',
+	'volunteer',
+	'mechanic',
+	'accounting',
+	'extra_shift',
+	'facilities',
+	'outreach',
+	'ordering',
+	'tools',
+	'other'
+]
+
+class Shoptime(Base):
+	__tablename__ = 'shoptime'
+	__table_args__ = (
+		ForeignKeyConstraint(['personID'], ['people.id'],
+			ondelete = 'CASCADE', onupdate = 'CASCADE'), 
+		{ 'mysql_engine' : 'InnoDB', 'mysql_charset' : 'utf8' }
+	)
+		
+	id		= Column(Integer, primary_key = True, unique = True, nullable = False)
+	personID	= Column(Integer, nullable = False, index = True)
+	start		= Column(DateTime, nullable = False)
+	duration	= Column(Time, nullable = False)
+	notes 		= Column(Unicode(200))
+	type 		= Column(MSEnum(*shoptimeChoices, strict = True),
+				nullable = False, index = True)
 	
-	def EmptyRow(self, table):
-		return RowObject(table, self.tables[table])
+class ShopOccupant(Base):
+	__tablename__ = 'shopOccupants'
+	__table_args__ = (
+		ForeignKeyConstraint(['personID'], ['people.id'],
+			ondelete = 'CASCADE', onupdate = 'CASCADE'), 
+		{ 'mysql_engine' : 'InnoDB', 'mysql_charset' : 'utf8' }
+	)
+		
+	id		= Column(Integer, primary_key = True, unique = True, nullable = False)
+	personID	= Column(Integer, nullable = False, index = True, unique = True)
+	start		= Column(DateTime, nullable = False)
+	type 		= Column(MSEnum(*shoptimeChoices, strict = True),
+				nullable = False, index = True)
+	
+bikeTypes = [
+	'Mountain',
+	'Road',
+	'Hybrid', 
+	'Cruiser',
+	'Three Speed',
+	'Recumbent',
+	'Chopper',
+	'Mixte',
+	'Tallbike',
+	'Town Bike',
+	'Touring'
+]
+				
+class Bike(Base):
+	__tablename__ = 'bikes'
+	__table_args__ = (
+		ForeignKeyConstraint(['personID'], ['people.id'],
+			ondelete = 'CASCADE', onupdate = 'CASCADE'), 
+		{ 'mysql_engine' : 'InnoDB', 'mysql_charset' : 'utf8' }
+	)
+		
+	id		= Column(Integer, primary_key = True, unique = True, nullable = False)
+	personID	= Column(Integer, index = True)
+	type		= Column(MSEnum(*bikeTypes, strict = True), nullable = False)
+	color		= Column(Unicode(64), nullable = False)
+	brand		= Column(Unicode(64))
+	model		= Column(Unicode(64))
+	serial		= Column(Unicode(64), nullable = False, index = True)
+
 
 def CreateTablesFromScratch():
-	print("Creating database tables from scratch...")
-	sys.stdout.flush()
+	print("Creating database tables...")
 	
-	conn = MySQLdb.connect(
-		host = "localhost",
-		user = "signin_creator",
-		passwd = "signin_creator",
-		db = "signin_db")
-		
-	cursor = conn.cursor()
-	cursor.execute("""SHOW TABLES""")
-	for table in cursor.fetchall():
-		cursor.execute("DROP TABLE {0}".format(table[0]))
+	engine = create_engine('mysql://signin_creator:signin_creator@localhost/signin_db?charset=utf8')
+	meta = MetaData()
+	meta.reflect(bind = engine)
+	meta.drop_all(bind = engine)
 	
-	cursor.execute("""CREATE TABLE people
-			(	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-				firstName VARCHAR(64) NOT NULL,
-				lastName VARCHAR(64),			
-			PRIMARY KEY(id),
-			UNIQUE INDEX(firstName, lastName),
-			INDEX(lastName) )
-			CHARSET=utf8 ENGINE=InnoDB;""")
-			
-	cursor.execute("""CREATE TABLE members
-			(	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-				personID INT UNSIGNED NOT NULL UNIQUE,
-				startDate DATE NOT NULL,
-				endDate DATE,
-				streetAddress VARCHAR(128),
-				emailAddress VARCHAR(64),
-				phoneNumber VARCHAR(32),
-				donation SMALLINT UNSIGNED,
-				notes VARCHAR(200),	
-			PRIMARY KEY(id),
-			INDEX(personID),
-			FOREIGN KEY(personID) REFERENCES people(id)
-				ON DELETE NO ACTION 
-				ON UPDATE CASCADE )
-			CHARSET=utf8 ENGINE=InnoDB;""")
-						
-	cursor.execute("""CREATE TABLE hours
-			(	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-				personID INT UNSIGNED NOT NULL,
-				start DATETIME NOT NULL,
-				duration TIME NOT NULL,
-				type ENUM(
-					'shoptime',
-					'parts',
-					'worktrade',
-					'volunteer',
-					'mechanic',
-					'accounting',
-					'extra_shift',
-					'facilities',
-					'outreach',
-					'ordering',
-					'tools',
-					'other') NOT NULL,
-				notes VARCHAR(200),
-			PRIMARY KEY(id),
-			INDEX(personID),
-			INDEX(type),
-			FOREIGN KEY(personID) REFERENCES people(id)
-				ON DELETE NO ACTION
-				ON UPDATE CASCADE )
-			CHARSET=utf8 ENGINE=InnoDB;""")
-			
-	cursor.execute("""CREATE TABLE peopleInShop LIKE hours""")
-	cursor.execute("""ALTER TABLE peopleInShop
-				DROP COLUMN duration,
-				DROP COLUMN notes,
-				ADD UNIQUE INDEX(personID);""")
-			
-	cursor.execute("""CREATE TABLE bikes
-			(	id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-				personID INT UNSIGNED,
-				type VARCHAR(64) NOT NULL,
-				color VARCHAR(64) NOT NULL,
-				brand VARCHAR(64),
-				model VARCHAR(64),
-				serial VARCHAR(64) NOT NULL,
-			PRIMARY KEY(id),
-			INDEX(personId),
-			INDEX(serial(10)),
-			FOREIGN KEY(personID) REFERENCES people(id)
-				ON DELETE NO ACTION
-				ON UPDATE CASCADE )
-			CHARSET=utf8 ENGINE=InnoDB;""")
-
-	conn.commit()
-	cursor.close()
-	conn.close()
+	Base.metadata.create_all(bind = engine)
 	
 	print("\tSuccess")
+
+_Session = None
+_engine = None
+session = None
+
+def Connect():
+	global _engine
+	global _Session
+	global session
+	
+	_engine = create_engine('mysql://signin:signin@localhost/signin_db?charset=utf8')
+	_Session = sessionmaker(bind = _engine)
+	session = _Session()
+
+Connect()
